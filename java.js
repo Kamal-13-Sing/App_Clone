ba54b4f26c05bedef377348fe6d8c77c36b0c092
kamalthapa <kamalthapa@gmail.com>
2023-04-09 09:28:03 +0545

    appclone

---
+++ java.js
@@ -0,0 +1,60 @@
+/*----------------------Satish JavaScript-------------------*/
+function show(){
+        document.getElementById("photo").style.display="none";
+    document.getElementById("photo1").style.display="block";
+}
+function hide(){
+    document.getElementById("photo1").style.display="none";
+        document.getElementById("photo").style.display="block";
+
+}
+
+/*--------------Kalpit Share button JavaScript---------------*/
+function share(){
+    document.getElementById("share").style.display="none";
+            document.getElementById("comment").style.display="block";
+}
+function unsharer(){
+        document.getElementById("comment").style.display="none";
+    document.getElementById("sharer").style.display="block";
+}
+
+/*----------------------Rajan JavaScript-------------------*/
+function like(){
+    document.getElementById("like").style.display="none";
+        document.getElementById("love").style.display="block";
+}
+function love(){
+            document.getElementById("love").style.display="none";
+    document.getElementById("like").style.display="block";
+}
+
+/*---------------Rajan Share button JavaScript---------------*/
+function sharer(){
+    document.getElementById("sharer").style.display="none";
+            document.getElementById("comment").style.display="block";
+}
+function unsharer(){
+        document.getElementById("comment").style.display="none";
+    document.getElementById("sharer").style.display="block";
+}
+
+/*---------------------menubar Java Script----------------------*/
+function seemore(){
+     document.getElementById("hidemenu").style.display="block";
+        document.getElementById("seemore").style.display="none";
+     document.getElementById("seeless").style.display="block";
+}
+function seeless(){
+    document.getElementById("seeless").style.display="none";
+        document.getElementById("seemore").style.display="block";
+    document.getElementById("hidemenu").style.display="none";
+}
+
+/*---------------- Java Script fo Help and Support--------------*/
+function help(){
+    document.getElementById("helpcentre").style.display="block";
+}
+function helpcenter(){
+    document.getElementById("helpcentre").style.display="none";
+}
